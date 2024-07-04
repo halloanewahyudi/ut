c107ea705b0ca38f0500744f2da5c6f6332567ed
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-07-04 23:38:03 +0700

    update-9

--- src/components/section/SectionOne.vue
+++ src/components/section/SectionOne.vue
@@ -16,7 +16,7 @@ const dataSlide = [
     {
         title: "loremdanipsum",
         subtitle: " Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        image: "img/s2.jpg"
+        image: "img/s3.webp"
     }
 ]
 
@@ -39,8 +39,8 @@ slide.mount()
                 <li class="pl-0 ml-0 glide__slide relative  mx-0 px-0 flex flex-col justify-center items-end pb-20 bg-cover bg-no-repeat text-white"
                     :style="{ backgroundImage: `url(${item.image})` }" v-for="item in dataSlide" :key="item">
                     <div class="layer absolute top-0 left-0 w-full h-full  opacity-30"></div>
-                    <div class="slide-text max-w-[880px] mx-auto mt-auto px-6 relative text-center flex flex-col gap-4">
-                        <h1 class="text-3xl lg:text-8xl font-bold"> {{ item.title }} </h1>
+                    <div class="slide-text max-w-[880px] mx-auto mt-auto p-6 lg:p-10 relative text-center flex flex-col gap-4 bg-member bg-opacity-40 rounded-lg backdrop-blur">
+                        <h1 class="text-3xl lg:text-5xl font-bold"> {{ item.title }} </h1>
                         <h4 class="text-xl lg:text-2xl">{{ item.subtitle }}</h4>
                     </div>
                 </li>
@@ -62,11 +62,11 @@ slide.mount()
     min-height: 100vh;
 }
 .glide__slide .slide-text {
-    @apply opacity-0 scale-110;
+    @apply opacity-0 scale-110 -translate-x-72;
 }
 
 .glide__slide.glide__slide--active .slide-text {
-    @apply opacity-100 scale-100 delay-100 duration-500;
+    @apply opacity-100 scale-100 translate-x-0 delay-100 duration-700;
 }
 .layer{
     background: rgb(0,0,0);
